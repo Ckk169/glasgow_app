ef066fa90d75cc53a9391f3b019a20e6d7182e3f
Christopher Boyle <mrcboyle@outlook.com>
2022-07-06 11:51:20 +0100

    css updates locations

--- client/src/components/LocationDetail.js
+++ client/src/components/LocationDetail.js
@@ -1,19 +1,28 @@
-import React from "react";
-
+import React, { useState } from "react";
+import '../css/LocationDetail.css'
 
 const LocationDetail = ({ location }) => {
 
-
-
     return (
-        //Need to apply appropriate tag and id here -SD
-        <>
+        <div className="location-container">
+            <div className="location-detail">
 
-            <h3>{location.name}</h3>
-            <h4>{location.title.locationDetail}</h4>
-            <p> {location.info}</p>
-            <img src={location.photo.past} />
-        </>
+                <div id="cf">
+                    <img className="bottom" src={location.photo.past} />
+                    <img className="top" src={location.photo.present} />
+                </div>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <h3>{location.name}</h3>
+                <h4>{location.title.locationDetail}</h4>
+                <p> {location.info}</p>
+
+            </div >
+        </div>
     )
-}
+};
 export default LocationDetail;
--- client/src/containers/LocationContainer.js
+++ client/src/containers/LocationContainer.js
@@ -2,37 +2,28 @@ import React, { useEffect, useState } from 'react';
 import LocationDetail from '../components/LocationDetail';
 import LeafletMap from '../components/LeafletMap';
 import LocationService from '../services/LocationService';
-import '../App.css';
+import '../css/LocationDetail.css'
 
+const LocationContainer = ({ locations }) => {
 
-const LocationContainer = ({locations}) => {
-
-    // const [locations, setLocations] = useState([]);
     const [selectedLocation, setSelectedLocation] = useState(null);
-console.log(locations)
-
-    // useEffect(() => {
-    //     LocationService.getLocations()
-    //         .then(locations => setLocations(locations))
-    // }, [])
 
     const showLocation = (locationId) => {
-    LocationService.showSelectedLocation(locationId)
-    .then(location => setSelectedLocation(location))
+        LocationService.showSelectedLocation(locationId)
+            .then(location => setSelectedLocation(location))
 
-    }
+    };
 
-
+    // classname added to match LocationDetails
     return (
-        <>
+        <div className="location-container">
 
             <LeafletMap locations={locations} showLocation={showLocation} />
             {selectedLocation ? <LocationDetail location={selectedLocation} /> : null}
 
-        </>
+        </div >
 
     )
-
-}
+};
 
 export default LocationContainer;
